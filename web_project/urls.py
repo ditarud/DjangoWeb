ee63160378a86abaa39e27aa1a337c21222b3729
ditarud <ditarud@miuandes.cl>
2019-09-04 15:36:32 -0400

    Fix4

--- web_project/urls.py
+++ web_project/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-                  path("", include("home.urls")),
+                  path('home/', include("home.urls")),
                   path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
